b75ff43a5b0a43675772fdea63bf9a9afc8d0ed3
Tahaniahz <tahani1nabiltahani@outlook.dk>
2024-08-28 12:34:47 +0200

    tahani

--- Tahani/java.js
+++ Tahani/java.js
@@ -1,8 +1,10 @@
+
+
 const filterButtons = document.querySelectorAll(".filter_buttons button");
 const filterableCards = document.querySelectorAll(".filterable_cards .card");
 
 const filterCards = (e) =>{
-    document.querySelector(".active").classList.remove(".active");
+    document.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
     console.log(e.target);
 
@@ -17,7 +19,19 @@ const filterCards = (e) =>{
 
 filterButtons.forEach(button => button.addEventListener("click", filterCards));
 
+// find alle img-elementer inde i .card div'er
+const images = document.querySelectorAll('.card img');
 
+// opret et tomt array til at gemme src-attributterne
+const imageSources = [];
+
+// gennemgå alle fundne billeder og tilføj deres src til arrayet
+images.forEach((imag) => {
+    imageSources.push(images.src);
+});
+
+// log imagesources array til konsollen for at verficere indholdet
+console.log(imageSources);
 
 
 
